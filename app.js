e8b59000e43993bb014defdd5f3bc8152bc0a5a0
RFKRV <l20220057@ssptaji.cz>
2024-10-03 22:10:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,33 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
- 
-function sayHello() {
-    document.getElementById('hello').innerText = 'Hello world';
+
+function vypocitat() {
+    const num1 = parseFloat(document.getElementById('num1').value); // Corrected 'Document' to 'document'
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operace = document.getElementById('operace').value;
+    let vysledek;
+
+    if (Nezadáno(num1) || Nezadáno(num2)) {
+        vysledek = "Chyba: Zadejte platná čísla.";
+    } else {
+        if (operace === '+') {
+            vysledek = num1 + num2;
+        } else if (operace === '-') {
+            vysledek = num1 - num2;
+        } else if (operace === '*') {
+            vysledek = num1 * num2;
+        } else if (operace === '/') {
+            if (num2 !== 0) {
+                vysledek = num1 / num2;
+            } else {
+                vysledek = "Chyba: Dělení nulou.";
+            }
+        } else {
+            vysledek = "Neznámá operace.";
+        }
+    }
+
+    document.getElementById('vysledek').innerText = "Výsledek: " + vysledek;
+    console.log(vysledek);
 }
